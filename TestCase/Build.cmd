deff410d778ca04e739f7db0f21fab53fe85bf16
MakiseKurisu <saberconer@gmail.com>
2013-07-21 18:34:48 -0500

    Clean up the folder

--- TestCase/Build.cmd
+++ TestCase/Build.cmd
@@ -1,1 +1,19 @@
-@call	..\Build.cmd
+@echo off
+pushd	%~dp0
+call	Kill.cmd
+
+cd	..
+cd	..
+
+cd	Waffle
+call	Build_Waffle.cmd
+call	Build_Waffle.cmd AMD64
+cd	..
+
+cd	Mojibake
+call	Build_Mojibake.cmd
+call	Build_Mojibake.cmd AMD64
+cd	..
+
+popd
+pause
